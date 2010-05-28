b6c01d8c1748193289514fe36b0742e8d74b0e6f
Alex Lobascio & Jay Phillips <pair+lobascio+jay@pivotallabs.com>
2010-05-28 11:06:38 -0700

    Resolving merge again with engineyard/ey-cloud-recipes master

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -33,7 +33,7 @@ end
 # require_recipe "sphinx"
 
 #uncomment to turn on memcached
-# require_recipe "memcached"
+ require_recipe "memcached"
 
 #uncomment to run the authorized_keys recipe
 #require_recipe "authorized_keys"
--- cookbooks/solr/recipes/default.rb
+++ cookbooks/solr/recipes/default.rb
@@ -2,7 +2,6 @@
 # Cookbook Name:: solr
 # Recipe:: default
 #
-<<<<<<< HEAD
 
 require 'digest/sha1'
 
@@ -16,30 +15,6 @@ node[:applications].each do |app,data|
   end
 
   directory "/var/run/solr" do
-=======
-# We specify what version we want below.
-solr_desiredversion = 1.4
-if ['solo', 'util'].include?(node[:instance_role])
-  if solr_desiredverison = 1.3
-    solr_file = "apache-solr-1.3.0.tgz"
-    solr_dir = "apache-solr-1.3.0"
-    solr_url = "http://mirror.its.uidaho.edu/pub/apache/lucene/solr/1.3.0/apache-solr-1.3.0.tgz"
-  else
-    solr_dir = "apache-solr-1.4.0"
-    solr_file = "apache-solr-1.4.0.tgz"
-    solr_url = "http://mirror.its.uidaho.edu/pub/apache/lucene/solr/1.4.0/apache-solr-1.4.0.tgz"
-  end
-
-  directory "/var/run/solr" do
-    action :create
-    owner node[:owner_name]
-    group node[:owner_name]
-    mode 0755
-  end
-
-  directory "/var/log/engineyard/solr" do
-    action :create
->>>>>>> engineyard/master
     owner node[:owner_name]
     group node[:owner_name]
     mode 0755
@@ -51,27 +26,17 @@ if ['solo', 'util'].include?(node[:instance_role])
     owner node[:owner_name]
     group node[:owner_name]
     mode 0755
-<<<<<<< HEAD
     variables({
       :rails_env => node[:environment][:framework_env]
     })
   end
 
   template "/etc/monit.d/solr.#{app}.monitrc" do
-=======
-  variables({
-    :rails_env => node[:environment][:framework_env]
-  })
-  end
-
-  template "/etc/monit.d/solr.monitrc" do
->>>>>>> engineyard/master
     source "solr.monitrc.erb"
     owner node[:owner_name]
     group node[:owner_name]
     mode 0644
     variables({
-<<<<<<< HEAD
       :app => app,
       :username => node[:owner_name]
     })
@@ -81,7 +46,7 @@ if ['solo', 'util'].include?(node[:instance_role])
     command("if [ ! -e /data/#{app}/jettyapps/solr ]; then cd /data/#{app}/jettyapps && " +
             "wget -O apache-solr-1.3.0.tgz http://mirror.cc.columbia.edu/pub/software/apache/lucene/solr/1.3.0/apache-solr-1.3.0.tgz && " +
             "tar -xzf apache-solr-1.3.0.tgz && " +
-            "mv apache-solr-1.3.0/example solr && " + 
+            "mv apache-solr-1.3.0/example solr && " +
             "rm -rf apache-solr-1.3.0; fi")
     action :run
   end
@@ -100,48 +65,4 @@ if ['solo', 'util'].include?(node[:instance_role])
     action :run
   end
 
-=======
-      :user => node[:owner_name],
-      :group => node[:owner_name]
-    })
-  end
-
-  remote_file "/data/#{solr_file}" do
-    source "#{solr_url}"
-    owner node[:owner_name]
-    group node[:owner_name]
-    mode 0644
-    backup 0
-    not_if { FileTest.exists?("/data/#{solr_file}") }
-  end
-
-  execute "unarchive solr-to-install" do
-    command "cd /data && tar zxf #{solr_file} && sync"
-    not_if { FileTest.directory?("/data/#{solr_dir}") }
-  end
-
-  execute "install solr example package" do
-    command "cd /data/#{solr_dir} && mv example /data/solr"
-    not_if { FileTest.exists?("/data/solr/start.jar") }
-  end
-
-   directory "/data/solr" do
-    action :create
-    owner node[:owner_name]
-    group node[:owner_name]
-    mode 0755
-  end
-
-   execute "chown_solr" do
-     command "chown #{node[:owner_name]}:#{node[:owner_name]} -R /data/solr"
-   end
-
-   execute "monit-reload" do
-     command "monit quit && telinit q"
-   end
-
-   execute "start-solr" do
-     command "sleep 3 && monit start solr_9080"
-   end
->>>>>>> engineyard/master
 end
